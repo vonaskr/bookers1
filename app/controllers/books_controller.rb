167ad474259f122708ee7368c011a828498c5c0c
EC2 Default User <ec2-user@ip-172-31-0-143.ec2.internal>
2022-02-27 14:29:25 +0000

    bug fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,17 +3,20 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
-  end
+  
   
   def index
     @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -24,9 +27,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
  private
    def book_params
-      params.require(:book).permit(:title, :body)
+      params.permit(:title, :body)
    end
 
 
